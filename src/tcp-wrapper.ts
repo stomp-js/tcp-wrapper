eaa4a542137bc74aaa4afb1eee81a4ff22db2c27
Deepak Kumar <deepak@kreatio.com>
2023-01-26 22:42:26 +0530

    Cleanup, API docs

--- src/tcp-wrapper.ts
+++ src/tcp-wrapper.ts
@@ -9,54 +9,98 @@ import {
  * Wrapper for a TCP socket to make it behave similar to the WebSocket interface
  */
 export class TCPWrapper implements IStompSocket {
+  /**
+   * @internal
+   */
   public readyState: StompSocketState;
-  public readonly url: string;
-  public binaryType: string = '';
 
-  public onclose: ((ev?: any) => any) | null = null;
-  public onerror: ((ev: any) => any) | null = null;
-  public onmessage: ((ev: IStompSocketMessageEvent) => any) | null = null;
-  public onopen: ((ev?: any) => any) | null = null;
+  /**
+   * @internal
+   */
+  public readonly url: string;
+
+  /**
+   * @internal
+   */
+  public onclose: (ev?: any) => any;
+
+  /**
+   * @internal
+   */
+  public onerror: (ev: any) => any;
+
+  /**
+   * @internal
+   */
+  public onmessage: (ev: IStompSocketMessageEvent) => any;
+
+  /**
+   * @internal
+   */
+  public onopen: (ev?: any) => any;
+
   private socket: Socket;
   private _closeEvtOnTermination: object | undefined;
 
+  /**
+   * Create a new instance
+   *
+   * @param host
+   * @param port
+   */
   constructor(host: string, port: number) {
     const noOp = () => {};
+
+    // NoOp callbacks to simplify logic
+    this.onclose = noOp;
+    this.onerror = noOp;
+    this.onmessage = noOp;
+    this.onopen = noOp;
 
     this.url = `tcp://${host}/${port}/`;
     this.readyState = StompSocketState.CONNECTING;
 
-    this.socket = createConnection(port, host, () => {
-      if (typeof this.onopen === 'function') {
-        this.readyState = StompSocketState.OPEN;
-        this.onopen();
-      }
+    this.socket = this.getSocket(port, host);
+
+    this.socket.on('connect', () => {
+      this.readyState = StompSocketState.OPEN;
+      this.onopen();
     });
 
     this.socket.on('data', ev => {
-      if (typeof this.onmessage === 'function') {
-        this.onmessage({ data: ev });
-      }
+      this.onmessage({ data: ev });
     });
 
     this.socket.on('close', hadError => {
       this.readyState = StompSocketState.CLOSED;
-      if (typeof this.onclose === 'function') {
-        if (this._closeEvtOnTermination) {
-          this.onclose(this._closeEvtOnTermination);
-        } else {
-          this.onclose({ wasClean: !hadError, type: 'CloseEvent', code: 1000 });
-        }
+      if (this._closeEvtOnTermination) {
+        this.onclose(this._closeEvtOnTermination);
+      } else {
+        this.onclose({ wasClean: !hadError, type: 'CloseEvent', code: 1000 });
       }
     });
 
     this.socket.on('error', ev => {
-      if (typeof this.onerror === 'function') {
-        this.onerror({ type: 'Error', error: 100, message: ev });
-      }
+      this.onerror({ type: 'Error', error: 100, message: ev });
     });
   }
 
+  /**
+   * In case you need to set custom options in the underlying TCP Socket,
+   * override this method.
+   *
+   * @param port
+   * @param host
+   * @protected
+   */
+  protected getSocket(port: number, host: string) {
+    return createConnection(port, host);
+  }
+
+  /**
+   * @internal
+   * @param data
+   */
   public send(data: string | ArrayBuffer) {
     if (typeof data === 'string') {
       this.socket.write(data);
@@ -65,11 +109,19 @@ export class TCPWrapper implements IStompSocket {
     }
   }
 
+  /**
+   * @internal
+   * @param code
+   * @param reason
+   */
   public close(code?: number, reason?: string) {
     this.readyState = StompSocketState.CLOSING;
     this.socket.end();
   }
 
+  /**
+   * @internal
+   */
   public terminate() {
     this.readyState = StompSocketState.CLOSING;
     this._closeEvtOnTermination = {
